917dc2f48d7e86ef702d1cd66bb870ffa4a9ea0c
RHam007 <hussars@gmail.com>
2025-02-14 17:54:48 -0800

    Added comments to clarify intent of each section

--- word_count.py
+++ word_count.py
@@ -2,12 +2,13 @@
 
 def word_count(word_to_count, sentence):
     count = 0
-    word = sentence.split()
-    for i in word:
+    word = sentence.split()  # Break the sentance string into individual words
+    for i in word:           # iterate thru the sentance string
         if i == word_to_count:
             count += 1
     print(f"The word '{word_to_count}' appears {count} times in the sentence.")
 
+# User defined variables w/prompts
 word_to_count = input("Word to count: ")
 sentence = input("Text to search: ")
 
