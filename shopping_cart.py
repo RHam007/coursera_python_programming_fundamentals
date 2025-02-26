d2525186ba931a7a049dc1143fb9829ce40a0f5c
RHam007 <hussars@gmail.com>
2025-02-25 16:57:55 -0800

    added shopping_cart.py
     - First code example is a possible working shopping cart segment taking user input.
     - Second code example is what the prompt wanted using direct .append() to simulate user input, but does not use the included function.

---
+++ shopping_cart.py
@@ -0,0 +1,25 @@
+# Functional cart snippet
+
+"""shopping_cart = []
+def add_item_to_cart(item):
+    shopping_cart.append(item)
+    for item in shopping_cart:
+        print(item)
+    return
+
+item = add_item_to_cart(input("Add which item?: "))"""
+
+# What the worksheet wants:
+
+"""shopping_cart = []
+def add_item_to_cart():
+    shopping_cart.append()
+    return
+
+shopping_cart.append("apple")
+shopping_cart.append("banana")
+shopping_cart.append("milk")
+
+print("Shopping Cart:")
+for item in shopping_cart:
+    print(item)"""
