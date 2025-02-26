008e6a39c81906a527af6d0ab136e66962b9973a
RHam007 <hussars@gmail.com>
2025-02-25 17:03:17 -0800

    added comments to clarify logic and code element intent

--- shopping_cart.py
+++ shopping_cart.py
@@ -11,15 +11,20 @@ item = add_item_to_cart(input("Add which item?: "))"""
 
 # What the worksheet wants:
 
+#initializing the shopping cart
 shopping_cart = []
+
+# defining the add_item_to_cart function
 def add_item_to_cart(item):
     shopping_cart.append(item)
     return
 
+# simulating user input by hardwiring the add_item_to_cart()
 add_item_to_cart("apple")
 add_item_to_cart("banana")
 add_item_to_cart("milk")
 
+# printing the shopping cart list
 print("Shopping Cart:")
 for item in shopping_cart:
     print(item)
