a17c9a9dd34e6e01326b060876188e03c5989e98
RHam007 <hussars@gmail.com>
2025-03-19 11:14:14 -0700

    updated panda_play.py to use example file names within the function calls The referenced file names DO NOT exist within the current project and are used as placeholders (comments included to clarify this within the code)

--- panda_play.py
+++ panda_play.py
@@ -1,5 +1,13 @@
 #Importing pandas
 import pandas as pd
 
-dataset1 = pd.read_csv("Iris.csv")
+# defining data set for the dataframe
+"""
+dataset1 = pd.read_csv("Iris.csv") #example data - file does not exist
 dataset1
+
+dataset2 = pd.read_csv("cars.csv")  #.read_csv("<file name>") loads the csv data into the 'dataset2' dataframe
+dataset2.head()  # provides a dataframe with only the first few rows to show the values and columns
+dataset2.info()  # returns the column name, a sum of the non-null field count, and the column's data type (Dtype)
+dataset2.describe() # returns basic analysis dataframe for each numerical column's data (count, mean, std, min, 25%/50%/75%, and max)
+"""
