e0e975a8d01cc5f49886a09b6d1fe0c6f799ab92
RHam007 <hussars@gmail.com>
2025-04-08 20:20:35 -0700

    Updated file_finder.py to remove personal information

--- automation_and_scripting_files/file_finder.py
+++ automation_and_scripting_files/file_finder.py
@@ -1,7 +1,7 @@
 import glob
 
-path_raw = r"C:\Users\LordKyron\Desktop" # Path as taken from Windows File Explorer (Use 'r' to signify this is a "raw" string for Windows)
-path_escaped = "C:\\\\Users\\\\LordKyron\\\\Desktop" # Exploded path (add 3 additional back-slashes "\" for each sub-folder tree change)
+path_raw = r"C:\target\file\path" # Path as taken from Windows File Explorer (Use 'r' to signify this is a "raw" string for Windows)
+path_escaped = "C:\\\\target\\\\file\\\\path" # Exploded path (add 3 additional back-slashes "\" for each sub-folder tree change)
 
 returned_files = glob.glob(path_raw + "/*.pdf") # Select ther desired file type(s)
 
